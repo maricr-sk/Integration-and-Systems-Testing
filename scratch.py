8019f4e9a518357505243ce28afd69ecd3133294
Marissa Crevecoeur <mac2538@columbia.edu>
2024-07-05 18:03:49 +0000

    lets see what this does -- added basic testing for pages and forms

---
+++ scratch.py
@@ -0,0 +1,5 @@
+from unittest.mock import Mock  # Imports Mock into project
+
+newMockObj = Mock()  # Creates a new mock object
+
+print(newMockObj)  #Print the object to the terminal for inspection
